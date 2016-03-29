60d52eccc1f058a755bcca1441cac643dfb07cb6
qinwf <mail@qinwenfeng.com>
2016-03-29 23:03:40 +0800

    Add: rust for appveyor

--- tests/testthat/test-message.r
+++ tests/testthat/test-message.r
@@ -1,2 +1,17 @@
 context("message")
 
+rusti = function(x){
+    rust(code = x ,depend = '
+     [dependencies]
+     rustr = {git = "https://github.com/rustr/rustr.git"}')
+}
+
+test_that("test message",{
+    skip_on_cran()
+    rusti('
+    // #[rustr_export]
+    pub fn say_warning(){
+        r_warn("test");
+    }')
+    expect_warning(say_warning(), "test")
+})
