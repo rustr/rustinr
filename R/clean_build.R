07f3be2c597c8f517f56843508b518792163be27
qinwf <mail@qinwenfeng.com>
2017-01-03 11:50:13 +0800

    Add: lib_only option for rustr_clean_build

--- R/clean_build.R
+++ R/clean_build.R
@@ -3,10 +3,25 @@
 #' \code{rustr_clean_build} cleans the build target of \code{cargo build}.
 #'
 #' @param pkgdir package path
+#' @param lib_only only remove builded static library
 #' @export
-rustr_clean_build <- function(pkgdir = ".") {
-  if(!dir.exists(file.path(pkgdir, "src", "rustlib", "target"))) {
-    warning(paste(as.character(file.path(pkgdir, "src", "rustlib", "target")), "is not exist."))
+rustr_clean_build <- function(pkgdir = ".", lib_only = FALSE) {
+  if(lib_only){
+
+      static_lib = file.path(pkgdir, "src", "rustlib", "target", "release", "librustlib.a")
+      if(!dir.exists(static_lib)) {
+          warning(paste(as.character(static_lib), "does not exist."))
+      }
+      unlink(static_lib)
+
+  } else{
+
+      target_dir = file.path(pkgdir, "src", "rustlib", "target")
+      if(!dir.exists(target_dir)) {
+          warning(paste(as.character(target_dir), "does not exist."))
+      }
+      unlink(target_dir, recursive = T)
+
   }
-  unlink(file.path(pkgdir, "src", "rustlib", "target"), recursive = T)
+
 }
